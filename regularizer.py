e40e5b9e6653a8cc6271ec5cbd238d95f9453839
samu.karli <samu.karli@limaeurope.hu>
2022-12-02 10:17:16 +0100

    SymPy

--- regularizer.py
+++ regularizer.py
@@ -1,24 +1,18 @@
 import csv
 import math
-from shapely import geometry as g
+# from shapely import geometry as g
 from matplotlib import pyplot as plt
 import uuid
-import pprint
+from sympy import geometry as g
 
 
-# class Tangent(float):
-#     EPS = 0.1
-#
-#     # def __eq__(self, other):
-#         # return
-
-
-class Point(g.Point):
+class Point(g.Point2D):
     EPS = 0.001
-    def __init__(self, p_x, p_y):
+    def __new__(cls, p_x, p_y):
         _x = float(p_x)
         _y = float(p_y)
-        super().__init__(_x, _y)
+        result = super().__new__(cls, _x, _y)
+        return result
 
     def __str__(self):
         return f"Point({self.x}, {self.y})"
@@ -28,21 +22,60 @@ class Point(g.Point):
         return self.x - EPS < other.x < self.x + EPS and self.y - EPS < other.y < self.y + EPS
 
 
-class Line(g.LineString):
+class Slope(float):
     EPS = 0.1
+
+    def __eq__(self, other):
+        EPS = Slope.EPS
+        _s = float(self)
+
+        if self > math.pi/2 - EPS and other < -math.pi/2 + EPS:
+            other += math.pi
+        if self < -math.pi/2 + EPS and other > math.pi/2 - EPS:
+            _s += math.pi
+
+        return _s - EPS < other < _s + EPS
+
+    def isParallel(self, p_other):
+        return self == p_other
+
+    def isAngleAt(self, p_other, p_angle, p_checkBothSides = False):
+        EPS = Slope.EPS
+        _s = float(self)
+
+        if p_other - _s <= -math.pi/2:
+            p_other += math.pi/2
+
+        result = p_angle -EPS <= p_other - _s <= p_angle + EPS
+
+        if p_checkBothSides:
+            if _s - p_other <= -math.pi / 2:
+                _s += math.pi / 2
+
+            result |= p_angle - EPS <= _s - p_other <= p_angle + EPS
+
+        return result
+
+    def isPerpendicular(self, p_other):
+        return self.isAngleAt(p_other, p_checkBothSides=True)
+
+
+class Line(g.Line):
+    EPS = 0.1
+
+    def __new__(cls, p_p1, p_p2):
+        _r = super(g.Line, cls).__new__(cls, p_p1, p_p2)
+        return _r
+
+    def __init__(self, p_p1: Point, p_p2: Point, ):
+        _list = [(p_p1.x, p_p1.y), (p_p2.x, p_p2.y), ]
+        self.guid = uuid.uuid4()
+        self.l1 = None
+        self.l2 = None
 
     @classmethod
     def setEPS(cls, p_eps):
         cls.EPS = p_eps
-
-    def __init__(self, p_p1:Point, p_p2:Point, ):
-        self.p1 = p_p1
-        self.p2 = p_p2
-        _list = [(p_p1.x, p_p1.y), (p_p2.x, p_p2.y), ]
-        super().__init__(_list)
-        self.guid = uuid.uuid4()
-        self.l1 = None
-        self.l2 = None
 
     def connect(self, p_line):
         if self.p1 == p_line.p1:
@@ -108,11 +141,32 @@ class Line(g.LineString):
 
 
 class CompositeLine(Line):
-    def __init__(self, p_seg:Line):
-        super().__init__(p_seg.p1, p_seg.p2)
+    def __new__(cls, p_seg:Line):
+        _r = super(g.Line, cls).__new__(cls, p_seg.p1, p_seg.p2)
+        return _r
+
+    def __init__(self, p_seg:Line, p_index = 0):
         self.lineList = [p_seg]
         self.p1 = p_seg.p1
         self.p2 = p_seg.p2
+        self.index = p_index
+        self.toBeUsed = True
+
+    @property
+    def p1(self):
+        return self._p1
+
+    @p1.setter
+    def p1(self, p_p1):
+        self._p1 = p_p1
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, p_p2):
+        self._p2 = p_p2
 
     def add(self, other:Line):
         if self.p1 == other.p1:
@@ -128,22 +182,36 @@ class CompositeLine(Line):
             self.p2 = other.p1
             self.lineList = self.lineList + [Line(other.p2, other.p1)]
 
+    def getPoint(self):
+        return self.length
 
-class ClosedPolyLine(g.LinearRing):
-    def __init__(self, p_list:list[Point]):
-        super(ClosedPolyLine, self).__init__([(p.x, p.y) for p in p_list])
+    def isNextTo(self, p_other):
+        return self.p2 == p_other.p1
+
+
+class ClosedPolyLine(g.Polygon):
+    def __new__(cls, p_list):
+        result = super().__new__(cls, *p_list)
+        return result
+
+    def __init__(self, p_list):
         self.compositeLines = []
         pPrev = p_list[0]
+        self.maxIndex = 0
+
         for p in p_list[1:]:
             seg = Line(pPrev, p)
 
             if len(self.compositeLines) and self.compositeLines[-1] == seg:
                 self.compositeLines[-1].add(seg)
             else:
-                self.compositeLines.append(CompositeLine(seg))
+                _cLine = CompositeLine(seg)
+                self.compositeLines.append(_cLine)
+                self.maxIndex += 1
             pPrev = p
 
     def toPlot(self):
+        '''For pyplot plotting'''
         pL = []
         cols = "rgbcymk"
         i = 0
@@ -152,96 +220,44 @@ class ClosedPolyLine(g.LinearRing):
             pL.append([cl.p1.y, cl.p2.y, ])
             pL.append(cols[i%len(cols)])
             i += 1
-
         return pL
 
+    # def autoPurge(self):
+    #     _compositeLinesOrdered = sorted(self.compositeLines, key=lambda i: i.length)
+    #     _compositeLinesOrdered[-1].toBeUsed = False
+    #     _compositeLinesOrdered = sorted(self.compositeLines, key=lambda i: i.index)
+    #
+    #     _prevCL = _compositeLinesOrdered[0]
+    #     for _CL in _compositeLinesOrdered[1:]:
+    #         if not _CL.toBeUsed:
+    #             continue
+    #
+    #         if not _CL.isNextTo(_prevCL):
+    #             _CL.intersect(_prevCL)
+    #             _prevCL.intersect(_CL)
+    #
+    #         _prevCL = _CL
 
-class Arc:
-    pass
-
-
-def getSimilarItems(p_item, p_fromItems: set = None):
-    result = set()
-    rest = set()
-
-    if not p_fromItems:
-        if p_item.l1 and p_item == p_item.l1:
-            result.add(p_item)
-        if p_item.l2 and p_item == p_item.l2:
-            result.add(p_item)
-    else:
-        for i in p_fromItems:
-            if p_item == i:
-                result.add(i)
-            else:
-                rest.add(i)
-
-    _result = set()
-    for i in result:
-        _r = getSimilarItems(i, rest)
-        _result |= _r
-        rest -= _r
-
-    result |= _result
-    result |= {p_item}
-    rest -= _result
-
-    return result
-
-def getClusters(p_listOfItems, p_listOfClusters):
-    if not p_listOfItems:
-        return
-
-    result = getSimilarItems(p_listOfItems[0])
-    rest = set(p_listOfItems) - result
-    p_listOfClusters.append(result)
-
-    getClusters(list(rest), p_listOfClusters)
 
 def readCSV(p_fileName):
     with open(p_fileName, "r") as csvFile:
-        lines = []
-
         _prevLine = None
 
-        # for row in csv.reader(csvFile):
-        #     _l = Line(Point((float)(row[0]), (float)(row[1])), Point((float)(row[2]), (float)(row[3])))
-        #     if _prevLine:
-        #         _prevLine.connect(_l)
-        #     lines.append(_l)
-        #     _prevLine = _l
-
-        Line.setEPS(0.3)
+        Line.setEPS(1)
 
         _pointList = []
+
         for row in csv.reader(csvFile):
-            _pointList.append(Point(row[0], row[1]))
+
+            _point = Point(row[0], row[1])
+            _pointList.append(_point)
 
         _pl = ClosedPolyLine(_pointList)
-        print(_pl.bounds)
+        # _pl.autoPurge()
 
         _p = _pl.toPlot()
         plt.axis('equal')
         plt.plot(*_p)
-
-        # listOfClusters = []
-        # getClusters(lines, listOfClusters)
-        #
-        # print(len(listOfClusters))
-        #
-        # listOfClusters  = filter(lambda l: len(l) > 1, listOfClusters)
-
-        # for c in listOfClusters:
-        #     _pl1 = []
-        #     _pl2 = []
-        #
-        #     i = 0
-        #
-        #     for l in c:
-        #         _pl1.append(l.distToPoint(Point(0,0)))
-        #         _pl2.append(l.slope())
-        #         plt.plot(_pl1, _pl2, cols[i % len(cols)] + "." )
-        #         i+=1
         plt.show()
 
 if __name__ == "__main__":
