157d2f22707daed4dd1242292871e6b65702cc5a
samu.karli <samu.karli@limaeurope.hu>
2022-12-06 14:00:01 +0100

    Refactor Reconnecting fixes

--- Classes/S_Line.py
+++ Classes/S_Line.py
@@ -4,55 +4,74 @@ import uuid
 from sympy import geometry as g
 
 
-class S_Line(g.Segment2D):
+class S_Line:
     EPS = 0.1
-
-    def __new__(cls, *args):
-        _r = super(S_Line, cls).__new__(cls, *args)
-        return _r
+    EPS_ANG = 0.01
 
     def __init__(self, p_p1: S_Point, p_p2: S_Point, ):
         _list = [(p_p1.x, p_p1.y), (p_p2.x, p_p2.y), ]
         self.guid = uuid.uuid4()
         self.l1 = None
         self.l2 = None
+        self._p1 = p_p1
+        self._p2 = p_p2
 
     def __str__(self):
-        return f"S_Line {self.p1} - {self.p2}"
+        return f"S_Line {self._p1} - {self._p2}"
 
     @classmethod
     def setEPS(cls, p_eps):
         cls.EPS = p_eps
 
+    @classmethod
+    def setAngularEPS(cls, p_eps):
+        cls.EPS_ANG = p_eps
+
+    @property
+    def p1(self):
+        return self._p1
+
+    @p1.setter
+    def p1(self, p_p1):
+        self._p1 = p_p1
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, p_p2):
+        self._p2 = p_p2
+
     def connect(self, p_line):
-        if self.p1 == p_line.p1:
+        if self._p1 == p_line.p1:
             self.l1 = p_line
             p_line.l1 = self
-        if self.p1 == p_line.p2:
+        if self._p1 == p_line.p2:
             self.l1 = p_line
             p_line.l2 = self
 
-        if self.p2 == p_line.p1:
+        if self._p2 == p_line.p1:
             self.l2 = p_line
             p_line.l1 = self
-        if self.p2 == p_line.p2:
+        if self._p2 == p_line.p2:
             self.l2 = p_line
             p_line.l2 = self
 
     def _dx(self)->float:
-        return self.p2.x - self.p1.x
+        return self._p2.x - self._p1.x
 
     def _dy(self)->float:
-        return self.p2.y - self.p1.y
+        return self._p2.y - self._p1.y
 
     def toList(self):
-        return [self.p1.x, self.p2.x], [self.p1.y, self.p2.y]
+        return [self._p1.x, self._p2.x], [self._p1.y, self._p2.y]
 
     def getLength(self)->float:
         return math.sqrt((self._dx()) ** 2 + (self._dy()) ** 2)
 
     def distToPoint(self, p_point:S_Point) -> float:
-        return (math.fabs((self._dx()) * (self.p1.y-p_point.y) - (self._dy()) * (self.p1.x-p_point.x)))  / self.getLength()
+        return (math.fabs((self._dx()) * (self._p1.y-p_point.y) - (self._dy()) * (self._p1.x-p_point.x)))  / self.getLength()
 
     def slope(self)->float:
         if math.fabs(self._dx()) > 0:
@@ -62,6 +81,7 @@ class S_Line(g.Segment2D):
 
     def __eq__(self, other):
         EPS = S_Line.EPS
+        EPS_ANG = S_Line.EPS_ANG
         _d1 = self.distToPoint(S_Point(0, 0))
         _d2 = other.distToPoint(S_Point(0, 0))
         _s1 = self.slope()
@@ -72,10 +92,78 @@ class S_Line(g.Segment2D):
             _s1 += math.pi
 
         return _d1 - EPS < _d2 < _d1 + EPS \
-        and _s1 - EPS < _s2 < _s1 + EPS
+        and _s1 - EPS_ANG < _s2 < _s1 + EPS_ANG
 
     def getSimilarLines(self, p_list:list, p_aEps, p_dEps)->list:
         return [l for l in p_list if l == self]
 
     def __hash__(self):
         return self.guid.int
+
+
+class S_CompositeLine(S_Line):
+    def __init__(self, *args, index = 0):
+        super().__init__(args[0].p1, args[0].p2)
+        if len(args) == 1:
+            if isinstance(args[0], S_Line):
+                self.lineList = [args[0]]
+                self._p1 = args[0].p1
+                self._p2 = args[0].p2
+        if len(args) == 2:
+            self.lineList = [S_Line(args[0], args[1])]
+            self._p1 = args[0]
+            self._p2 = args[1]
+        self.index = index
+        self.toBeUsed = True
+        self.added = False
+        self.segment = g.Segment2D(self.p1, self.p2)
+
+    def __repr__(self):
+        return f"CL {'O' if self.toBeUsed else 'X'}:({float(self.p1.x)},{float(self.p1.y)})->({float(self.p2.x)},{float(self.p2.y)})"
+
+    @property
+    def p1(self):
+        return self._p1
+
+    @p1.setter
+    def p1(self, p_p1):
+        self._p1 = p_p1
+        self.segment = g.Segment2D(self._p1, self._p2)
+
+    @property
+    def p2(self):
+        return self._p2
+
+    @p2.setter
+    def p2(self, p_p2):
+        self._p2 = p_p2
+        self.segment = g.Segment2D(self._p1, self._p2)
+
+    def add(self, other:S_Line):
+        if self.p1 == other.p1:
+            self.p1 = other.p2
+            self.lineList = [S_Line(other.p2, other.p1)] + self.lineList
+        elif self.p1 == other.p2:
+            self.p1 = other.p1
+            self.lineList = [S_Line(other.p1, other.p2)] + self.lineList
+        elif self.p2 == other.p1:
+            self.p2 = other.p2
+            self.lineList = self.lineList + [S_Line(other.p1, other.p2)]
+        elif self.p2 == other.p2:
+            self.p2 = other.p1
+            self.lineList = self.lineList + [S_Line(other.p2, other.p1)]
+
+    def getPoint(self):
+        return self.segment.length
+
+    def isNextTo(self, p_other):
+        return self.p1 == p_other.p2
+
+    def toLine(self):
+        _r = g.Line(self.p1, self.p2)
+        return _r
+
+    @property
+    def order(self):
+        return self.segment.length
+
--- regularizer.py
+++ regularizer.py
@@ -4,81 +4,10 @@ from matplotlib import pyplot as plt
 from sympy import geometry as g
 
 from Classes.S_Point import S_Point
-from Classes.S_Line import S_Line
+from Classes.S_Line import S_Line, S_CompositeLine
 
 
-class S_CompositeLine(S_Line):
-    def __new__(cls, *args):
-        if isinstance(args[0], S_Line):
-            _r = super(S_Line, cls).__new__(cls, args[0].p1, args[0].p2)
-        else:
-                _r = super(S_Line, cls).__new__(cls, *args)
-        return _r
-
-    def __init__(self, *args, p_index = 0):
-        super(g.Segment, self).__init__()
-        if len(args) == 1:
-            if isinstance(args[0], S_Line):
-                self.lineList = [args[0]]
-                self.p1 = args[0].p1
-                self.p2 = args[0].p2
-        if len(args) == 2:
-            self.lineList = [S_Line(args[0], args[1])]
-            self.p1 = args[0]
-            self.p2 = args[1]
-        self.index = p_index
-        self.toBeUsed = True
-        self.added = False
-
-    def __repr__(self):
-        return f"CL {'O' if self.toBeUsed else 'X'}:({float(self.p1.x)},{float(self.p1.y)})->({float(self.p2.x)},{float(self.p2.y)})"
-
-    @property
-    def p1(self):
-        return self._p1
-
-    @p1.setter
-    def p1(self, p_p1):
-        self._p1 = p_p1
-
-    @property
-    def p2(self):
-        return self._p2
-
-    @p2.setter
-    def p2(self, p_p2):
-        self._p2 = p_p2
-
-    def add(self, other:S_Line):
-        if self.p1 == other.p1:
-            self.p1 = other.p2
-            self.lineList = [S_Line(other.p2, other.p1)] + self.lineList
-        elif self.p1 == other.p2:
-            self.p1 = other.p1
-            self.lineList = [S_Line(other.p1, other.p2)] + self.lineList
-        elif self.p2 == other.p1:
-            self.p2 = other.p2
-            self.lineList = self.lineList + [S_Line(other.p1, other.p2)]
-        elif self.p2 == other.p2:
-            self.p2 = other.p1
-            self.lineList = self.lineList + [S_Line(other.p2, other.p1)]
-
-    def getPoint(self):
-        return self.length
-
-    def isNextTo(self, p_other):
-        return self.p1 == p_other.p2
-
-    def toLine(self):
-        _r = g.Line(self.p1, self.p2)
-        return _r
-
-    @property
-    def order(self):
-        return self.length
-
-
-class S_ClosedPolyLine():
+class S_ClosedPolyLine:
     def __init__(self, p_list):
         self.compositeLines = []
         pPrev = p_list[0]
@@ -91,13 +20,14 @@ class S_ClosedPolyLine():
             if len(self.compositeLines) and self.compositeLines[-1] == seg:
                 self.compositeLines[-1].add(seg)
             else:
-                _cLine = S_CompositeLine(seg)
+                _cLine = S_CompositeLine(seg, index=self.maxIndex)
                 self.compositeLines.append(_cLine)
                 self.maxIndex += 1
             pPrev = p
         if pPrev != p_list[0]:
-            _line = S_CompositeLine(S_Line(pPrev, p_list[0]))
+            _line = S_CompositeLine(S_Line(pPrev, p_list[0]), index=self.maxIndex)
             self.compositeLines.append(_line)
+            self.maxIndex += 1
 
     @property
     def polygon(self):
@@ -107,7 +37,7 @@ class S_ClosedPolyLine():
         return _p
 
     def toPlot(self):
-        '''For pyplot plotting'''
+        """For pyplot plotting"""
         pL = []
         cols = "rgbcymk"
         i = 0
@@ -119,16 +49,25 @@ class S_ClosedPolyLine():
         return pL
 
     def reconnectAllEdges(self):
-        _prevCL = None
+        _compositeLines = list(filter(lambda cl: cl.toBeUsed, self.compositeLines))
+        _unusedLines = filter(lambda cl: not cl.toBeUsed, self.compositeLines)
 
-        for _CL in [*self.compositeLines, *self.compositeLines, ]:
-            if not _CL.toBeUsed:
-                continue
-            elif _prevCL and not _CL.isNextTo(_prevCL):
+        _prevCL = _compositeLines[-1]
+
+        for _CL in _compositeLines:
+            if not _CL.isNextTo(_prevCL):
                 p = _CL.toLine().intersection(_prevCL.toLine())[0]
                 _prevCL.p2 = S_Point(p.x, p.y)
                 _CL.p1 = S_Point(p.x, p.y)
-            _prevCL = _CL
+                if _CL.getLength() == 0:
+                    _CL.toBeUsed = False
+                else:
+                    _prevCL = _CL
+            else:
+                _prevCL = _CL
+
+        lines_ = [*_compositeLines, *_unusedLines]
+        self.compositeLines = lines_
 
     def removeOneSegmentAndReconnect(self):
         _compositeLinesOrdered = sorted(self.compositeLines, key=lambda i: i.order)
@@ -161,6 +100,7 @@ def readCSV(p_fileName):
             _pointList.append(_point)
 
         S_Line.setEPS(1)
+        S_Line.setAngularEPS(0.1)
         _pl = S_ClosedPolyLine(_pointList)
         print(1)
         _pl.autoPurge()
